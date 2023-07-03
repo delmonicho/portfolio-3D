2f17d9a8a4186accc24dd5738f00fe3a75bdb4b9
Nicho Delmo <ndelmoral13@gmail.com>
2023-07-03 14:00:24 -0400

    add work details and logos

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import advicent from "./company/advicent.jpg";
+import casablanca from "./company/casablanca.jpg";
+import nexient from "./company/nexient.jpg";
+import utilisim from "./company/utilisim.jpg";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +52,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  advicent,
+  casablanca,
+  nexient,
+  utilisim,
   carrent,
   jobit,
   tripguide,
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
 
 const Hero = () => {
   return (
@@ -42,4 +43,4 @@ const Hero = () => {
 </section>  )
 }
 
-export default Hero
+export default SectionWrapper(Hero, '')
